5e3e1ac8d30ad79e4642e1bafc4f892e920c98c9
Simon-E-Mapp <3ggapp24.baugsi@folkuniversitetet.nu>
2025-09-25 13:42:37 +0200

    finishing

--- src/main.jsx
+++ src/main.jsx
@@ -5,23 +5,27 @@ import App from './App.jsx'
 import {createHashRouter, RouterProvider} from "react-router";
 import Breeds from './Components/Breeds.jsx';
 import BreedsExtended from './Components/BreedsExtended.jsx'
+import Frontpage from './Components/Frontpage.jsx';
 
 
 
   const router = createHashRouter([
-    {
-      path: "/",
-      Component: App,
-    },
-    {
-      path: "/Breeds",
-      Component: Breeds,
-    },
-    {
-      path: "/BreedsExtended",
-      Component: BreedsExtended,
-    },
-  ]);
+     {
+    path: "/",
+    element: <App />, 
+    children: [
+      {index: true,
+        element: <Frontpage />
+      },
+      { path: "Breeds", 
+        element: <Breeds /> 
+      },
+      { path: "BreedsExtended", 
+        element: <BreedsExtended /> 
+      },
+    ],
+  },
+]);
 
 
 createRoot(document.getElementById('root')).render(
